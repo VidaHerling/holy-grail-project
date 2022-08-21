b2ded328c211d5c8a93ec6b8c6aa0fa585b57c5b
Vida <herling.codingcamp@gmail.com>
2022-08-20 21:54:09 -0400

    add redis, get data and update data function

--- index.js
+++ index.js
@@ -1,11 +1,59 @@
 const express = require('express');
 const app = express();
+const redis = require('redis');
+const client = redis.createClient();
+
+// initialize values
+client.mset('header', 0, 'left', 0, 'article', 0, 'right', 0, 'footer', 0);
+client.mget(['header', 'left', 'article', 'right', 'footer'],
+  (err, value) => {
+    console.log(value);
+  });
+
+function data(){
+  return new Promise((resolve, reject) => {
+    client.mget(['header', 'left', 'article', 'right', 'footer'],
+    (err, value) => {
+      const data = {
+        'header': Number(value[0]),
+        'left': Number(value[1]),
+        'article': Number(value[2]),
+        'right': Number(value[3]),
+        'footer': Number(value[4])
+      };
+      err ? reject(null) : resolve(data);
+    });
+  });
+}
 
 // serve static files from public directory
 app.use(express.static('public'));
 
-app.get('/', (req, res)=>{
-  res.send("Hello World");
+//get data
+app.get('/data', (req, res) => {
+  data()
+    .then(data => {
+      console.log(data);
+      res.send(data);
+    })
+})
+
+//update data
+app.get('/update/:key/:value', (req, res) => {
+  const key = req.params.key;
+  let value = Number(req.params.value);
+  client.get(key, (err, reply) => {
+    // new value
+    value = Number(reply) + value;
+    client.set(key, value);
+
+    //return data to client
+    data()
+      .then(data => {
+        console.log(data);
+        res.send(data);
+      })
+  })
 })
 
 app.listen(3000, () => {
